1279a5a412954033855d890dd35b7c1248d10113
Olivia LaVal <oj.laval@gmail.com>
2022-11-02 21:44:59 -0500

    Add pkcs padding - doesn't work

--- CryptoMidtermProject.cpp
+++ CryptoMidtermProject.cpp
@@ -1,5 +1,4 @@
 // CryptoMidtermProject.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
 
 #include <iostream>
 #include <fstream>
@@ -11,20 +10,33 @@ using namespace std;
 using namespace boost::multiprecision;
 
 //Function to find gcd(a,b) - from GeeksForGeeks
-int1024_t gcd(int1024_t a, int1024_t b);
+cpp_int gcd(cpp_int a, cpp_int b);
 
 // Function for extended Euclidean Algorithm - from GeeksForGeeks
-int1024_t gcdExtended(int1024_t a, int1024_t b, int1024_t* x, int1024_t* y);
+cpp_int gcdExtended(cpp_int a, cpp_int b, cpp_int* x, cpp_int* y);
 
 // Function to find modulo inverse of a - from GeeksForGeeks
-int1024_t modInverse(int1024_t A, int1024_t M);
+cpp_int modInverse(cpp_int A, cpp_int M);
 
-int1024_t encrypt(int1024_t x, int1024_t e, int1024_t n);
+cpp_int encrypt(cpp_int x, cpp_int e, cpp_int n);
 
-int1024_t decrypt(int1024_t c, int1024_t d, int1024_t n);
+cpp_int decrypt(cpp_int c, cpp_int d, cpp_int n);
+
+cpp_int encode_pkcs(cpp_int x, cpp_int n);
+
+cpp_int decode_pkcs(cpp_int c, cpp_int n);
 
 //Iterative Function to calculate (x^y)%p in O(log y) - from GeeksForGeeks
-int1024_t powerMod(int1024_t x, int1024_t y, int1024_t p);
+cpp_int powerMod(cpp_int x, cpp_int y, cpp_int p);
+
+//Function to turn a int into binary string - found on stack overflow
+string toBinary(cpp_int n);
+
+// Function to convert binary to decimal - from GeeksForGeeks
+cpp_int binaryToDecimal(string binaryString);
+
+// Function to generate a random binary string of length N - from GeeksForGeeks
+string generateBinaryString(int n);
 
 int main()
 {
@@ -35,7 +47,7 @@ int main()
     cin >> inputpqefilename;
 
     //Read p,q,e from the file
-    int1024_t p, q, e;
+    cpp_int p, q, e;
 
     ifstream inputpqefile(inputpqefilename);
     if (inputpqefile) {
@@ -59,12 +71,14 @@ int main()
         return 0;
     }
 
-    int1024_t phi = 0;
-    phi = (p - 1) * (q - 1);
+    cpp_int phi = 0;
+    cpp_int pmin1 = p - 1;
+    cpp_int qmin1 = q - 1;
+    phi = pmin1 * qmin1;
     cout << "phi = " << phi << "\n";
 
     //Increments e until e is relative prime to phi
-    int1024_t track;
+    cpp_int track;
     while (e < phi) {
         track = gcd(e, phi);
         if (track == 1)
@@ -78,7 +92,7 @@ int main()
     cin >> outputNdfilename;
 
     //calculate d and N and store in the output file
-    int1024_t n = 0, d = 0;
+    cpp_int n = 0, d = 0;
     n = p * q;
     d = modInverse(e, phi);
 
@@ -99,7 +113,7 @@ int main()
     string inputxfilename;
     cin >> inputxfilename;
 
-    int1024_t x = 0;
+    cpp_int x = 0;
     ifstream inputxfile(inputxfilename);
     if (inputxfile.is_open()) {
         string drop;
@@ -122,14 +136,17 @@ int main()
     cin >> outputEfilename;
 
     //caluate E(x) and store in the ouput file
-    int1024_t ex = 0;
+    cpp_int ex = 0;
     ex = encrypt(x, e, n);
-
     cout << "E(x) = " << ex << "\n";
+
+    cpp_int ex_pad = encrypt(encode_pkcs(x, n), e, n);
+    cout << "E(encodeX) = " << ex_pad << "\n";
 
     ofstream outputEfile(outputEfilename);
     if (outputEfile.is_open()) {
         outputEfile << "E(x) = " << ex << "\n";
+        outputEfile << "E(encodeX) = " << ex_pad << "\n";
         outputEfile.close();
     }
     else {
@@ -141,7 +158,8 @@ int main()
     string inputcfilename;
     cin >> inputcfilename;
 
-    int1024_t c = 0;
+    cpp_int c = 0;
+    cpp_int c_pad = 0;
     ifstream inputcfile(inputcfilename);
     if (inputcfile.is_open()) {
         string drop;
@@ -149,6 +167,10 @@ int main()
         inputcfile >> drop;
         inputcfile >> drop;
         inputcfile >> c;
+
+        inputcfile >> drop;
+        inputcfile >> drop;
+        inputcfile >> c_pad;
 
         cout << "c = " << c << "\n";
         inputcfile.close();
@@ -164,14 +186,17 @@ int main()
     cin >> outputDfilename;
 
     //calculate D(c) and store in the output file
-    int1024_t dc = 0;
+    cpp_int dc = 0;
     dc = decrypt(c, d, n);
-
     cout << "D(c) = " << dc << "\n";
+
+    cpp_int dc_pad = decrypt(decode_pkcs(c_pad, n), d, n);
+    cout << "D(decodeC) = " << dc_pad << "\n";
 
     ofstream outputDfile(outputDfilename);
     if (outputDfile.is_open()) {
         outputDfile << "D(c) = " << dc << "\n";
+        outputDfile << "D(decodeC) = " << dc_pad << "\n";
         outputDfile.close();
     }
     else {
@@ -181,8 +206,8 @@ int main()
 
 }
 
-int1024_t gcd(int1024_t a, int1024_t b) {
-    int1024_t t;
+cpp_int gcd(cpp_int a, cpp_int b) {
+    cpp_int t;
     while (1) {
         t = a % b;
         if (t == 0)
@@ -192,21 +217,23 @@ int1024_t gcd(int1024_t a, int1024_t b) {
     }
 }
 
-int1024_t modInverse(int1024_t A, int1024_t M) {
-    int1024_t x, y;
-    int1024_t g = gcdExtended(A, M, &x, &y);
+cpp_int modInverse(cpp_int A, cpp_int M) {
+    cpp_int x, y;
+    cpp_int g = gcdExtended(A, M, &x, &y);
     if (g != 1) {
         cout << "Inverse doesn't exist";
         return 0;
     }
     else {
         // m is added to handle negative x
-        int1024_t res = (x % M + M) % M;
+        cpp_int res = x % M;
+        res = res + M;
+        res = res % M;
         return res;
     }
 }
 
-int1024_t gcdExtended(int1024_t a, int1024_t b, int1024_t* x, int1024_t* y) {
+cpp_int gcdExtended(cpp_int a, cpp_int b, cpp_int* x, cpp_int* y) {
     // Base Case
     if (a == 0) {
         *x = 0, * y = 1;
@@ -214,8 +241,8 @@ int1024_t gcdExtended(int1024_t a, int1024_t b, int1024_t* x, int1024_t* y) {
     }
 
     // To store results of recursive call
-    int1024_t x1, y1;
-    int1024_t gcd = gcdExtended(b % a, a, &x1, &y1);
+    cpp_int x1, y1;
+    cpp_int gcd = gcdExtended(b % a, a, &x1, &y1);
 
     // Update x and y using results of recursive
     // call
@@ -225,17 +252,55 @@ int1024_t gcdExtended(int1024_t a, int1024_t b, int1024_t* x, int1024_t* y) {
     return gcd;
 }
 
-int1024_t encrypt(int1024_t x, int1024_t e, int1024_t n) {
-    return (int1024_t)powerMod(x, e, n);
+cpp_int encrypt(cpp_int x, cpp_int e, cpp_int n) {
+    return (cpp_int)powerMod(x, e, n);
 }
 
-int1024_t decrypt(int1024_t c, int1024_t d, int1024_t n) {
-    return (int1024_t)powerMod(c, d, n);
+cpp_int decrypt(cpp_int c, cpp_int d, cpp_int n) {
+    return (cpp_int)powerMod(c, d, n);
 }
 
-int1024_t powerMod(int1024_t x, int1024_t y, int1024_t p)
+cpp_int encode_pkcs(cpp_int x, cpp_int n) {
+    unsigned int lengthX = msb(x);
+    unsigned int lengthN = msb(n);
+    if (lengthX <= lengthN - 11) {
+        unsigned int lengthR = (lengthN/8 - lengthX/8 - 3);
+
+        string encodedX = "00000010" + generateBinaryString(lengthR) + "0000" + toBinary(x);
+
+        return binaryToDecimal(encodedX);
+    }
+    else {
+        cout << "Error in encode_pkcs\n";
+        return 0;
+    }
+}
+
+cpp_int decode_pkcs(cpp_int c, cpp_int n) {
+    unsigned int lengthC = msb(c);
+    unsigned int lengthN = msb(n);
+    if (lengthC < lengthN) {
+        string binaryString = toBinary(c);
+
+        int i = 8;
+        //Find how many bits before the 4 zeros
+        int count = 0;
+        while (i < lengthN) {
+            if (binaryString[i] == '0') {
+                count++;
+            }
+            if (count == 4) {
+                break;
+            }
+        }
+        string decoded = binaryString.substr(i);
+        return binaryToDecimal(decoded);
+    }
+}
+
+cpp_int powerMod(cpp_int x, cpp_int y, cpp_int p)
 {
-    int1024_t res = 1;     // Initialize result
+    cpp_int res = 1;     // Initialize result
 
     x = x % p; // Update x if it is more than or
     // equal to p
@@ -253,4 +318,65 @@ int1024_t powerMod(int1024_t x, int1024_t y, int1024_t p)
         x = (x * x) % p;
     }
     return res;
+}
+
+string toBinary(cpp_int n)
+{
+    string r;
+    while (n != 0) {
+        r = (n % 2 == 0 ? "0" : "1") + r;
+        n /= 2;
+    }
+    return r;
+}
+
+// Function to convert binary to decimal
+cpp_int binaryToDecimal(string binaryString) {
+    string b = binaryString;
+    cpp_int dec_value = 0;
+
+    // Initializing base value to 1, i.e 2^0
+    cpp_int base = 1;
+
+    size_t i = b.length();
+    while (i > 0) {
+        char last_bit = b[i];
+        i--;
+
+        if (last_bit == '1') {
+            dec_value += 1 * base;
+        }
+
+        base = base * 2;
+    }
+
+    return dec_value;
+}
+
+string generateBinaryString(int n) {
+    srand(time(NULL));
+    string s = "";
+
+    // Iterate over the range [0, N - 1]
+    for (int i = 0; i < n; i++) {
+
+        // Store the random number
+        int x = ((int)rand() % 2);
+
+        // Append it to the string
+        s += to_string(x);
+    }
+
+    //Make sure there are never 4 zeros in a row
+    int count = 0;
+    for (int i = 0; i < n; i++) {
+        if (s[i] == '0') {
+            count++;
+        }
+        if (count == 4) {
+            s[i] = '1';
+        }
+    }
+
+    return s;
 }
